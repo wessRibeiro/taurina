099aea730c49312a49378dcbe4a8644186480347
Weslley Ribeiro <wess_ribeiro@hotmail.com>
2017-09-13 01:19:23 -0300

    teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Agência Taurina
+                    Agência Taurina :)
                 </div>
 
            
